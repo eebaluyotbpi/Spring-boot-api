2f151dcb4871d14fcba918519459f64792316934
Ericson <eebaluyotbpi@gmail.com>
2025-12-25 20:47:13 +0800

    Spring Boot API Tutorial

---
+++ src/main/java/com/autumncode/SoftwareEngineerController.java
@@ -0,0 +1,34 @@
+package com.autumncode;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineer")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineersById( @PathVariable Integer id) {
+        return softwareEngineerService.getEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(
+            @RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+
+
+}
---
+++ src/main/java/com/autumncode/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.autumncode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
