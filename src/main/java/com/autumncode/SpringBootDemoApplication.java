715e651c7e6611ef05c44958ef0a76937088de64
Ericson <eebaluyotbpi@gmail.com>
2025-12-25 20:50:01 +0800

    Spring Boot API Tutorial

--- src/main/java/com/autumncode/SpringBootDemoApplication.java
+++ src/main/java/com/autumncode/SpringBootDemoApplication.java
@@ -12,9 +12,4 @@ public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
-
-    @GetMapping
-    public String helloworld() {
-        return "Hello World!";
-    }
 }
